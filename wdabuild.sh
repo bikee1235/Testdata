c316af2bd647f65bd286678305d7f4f9875bf7dc
bikee1235 <bikee.bihari@sstsinc.com>
2024-04-10 16:56:45 +0530

    updated

--- wdabuild.sh
+++ wdabuild.sh
@@ -1,3 +1,5 @@
 ./resign.sh AgentBuild/Build/Products/Debug-iphoneos/Agent-Runner.app
 ./resign.sh Source13.4Build/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app
 ./resign.sh AppiumWebDriverBuild/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app
+./resign.sh WebDriverBuild13.4/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app
+./resign.sh WebDriverBuild/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app
